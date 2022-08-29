6e66b5005f4907818600c8db633092959efc72dd
Lucie-Baborova <lucie.babor@gmail.com>
2022-08-29 23:05:00 +0200

    setting up react-leaflet map

--- Desktop/Coding Activities/restaurantsMap/client/src/App.js
+++ Desktop/Coding Activities/restaurantsMap/client/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
   return (
-    <div className="App">
-       <div id="map"></div>
-    </div>
+    <MapContainer className="RestaurantsMap" 
+                  center={[47.373878, 8.545094]} 
+                  zoom={5} 
+                  scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 

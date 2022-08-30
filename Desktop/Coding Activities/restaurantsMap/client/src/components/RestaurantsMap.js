345fe62b251d4ece853af640eaf8b281e1845c1b
Lucie-Baborova <lucie.babor@gmail.com>
2022-08-30 21:29:03 +0200

    styling updates

--- Desktop/Coding Activities/restaurantsMap/client/src/components/RestaurantsMap.js
+++ Desktop/Coding Activities/restaurantsMap/client/src/components/RestaurantsMap.js
@@ -10,7 +10,7 @@ function RestaurantsMap(props) {
                   scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://api.mapbox.com/styles/v1/luciebbr/cl7gjuddn001i15p8mip0g9hd/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVjaWViYnIiLCJhIjoiY2w3Z2pveHB4MDVieTNubzBnYnFmaWlsOSJ9.3lJjKxP4RT-q_Ush43Vf_g"
       />
        {
          props.restaurants.map(r => (
@@ -19,6 +19,7 @@ function RestaurantsMap(props) {
                             { r.restaurant_name } <br /> 
                             { r.restaurant_description } <br /> 
                             { `${r.street} ${r.housenum}, ${r.city}, ${r.country}` }
+                            <img src=""></img>
                         </Popup>
                     </Marker>
                 ))
